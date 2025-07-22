2b6446cd6094836fb0ab0d1e1b93032d26a30fc9
Alexander Peebles <alexanderpeebles@hotmail.com>
2025-07-22 17:27:03 -0400

    pictures fixed

--- src/pages/Directory.tsx
+++ src/pages/Directory.tsx
@@ -7,7 +7,7 @@
         <h1 className="text-center text-2xl font-bold mb-4 underline">Who am I?</h1>
         <div className="rounded-2xl p-6 mb-2 border-2 ">
           <div className="flex items-center ">
-            <img src="src\assets\suit-cropped.jpg" alt="dapper" className="mx-5 mb-4 h-100 border-1 object-cover rounded-lg" />
+            <img src="\assets\suit-cropped.jpg" alt="dapper" className="mx-5 mb-4 h-100 border-1 object-cover rounded-lg" />
             <p className=" font-semibold text-xl">Hey thanks for visiting my site, my names Alex. I am an aspiring developer that went to school at Georgian College. With experience in programming and related hardware, I also have some experience in game development and business technologies. I hope you will consider my employment!</p>
           </div>
         </div>
@@ -23,7 +23,7 @@
             <h2 className="text-center text-2xl font-bold mb-4 underline">Due to this website being publicly deployed</h2>
             <h3 className="text-center text-xl font-semibold mb-4">Refer to either the resume or business card that was provided to you to get in touch with me</h3>
             <h4 className="text-center text-xl font-semibold">Thanks</h4>
-            <img src="src\assets\peepoWICKED.png" alt="WICKED" className="mx-auto mb-4 h-auto" />
+            <img src="\assets\peepoWICKED.png" alt="WICKED" className="mx-auto mb-4 h-auto" />
           </div> 
       </div>
     );
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -7,7 +7,7 @@ const Projects = () => {
       {/* Project 1 */}
       <div className="flex flex-col items-center max-w-sm">
         <img
-          src="src/assets/CardCaptor.png"
+          src="/assets/CardCaptor.png"
           alt="card captor"
           className="w-80 h-80 object-cover rounded-lg mb-4"
         />
@@ -29,7 +29,7 @@ const Projects = () => {
       {/* Project 2 */}
       <div className="flex flex-col items-center max-w-sm">
         <img
-          src="src/assets/suit-cropped.jpg"
+          src="/assets/suit-cropped.jpg"
           alt="dapper"
           className="w-80 h-80 object-cover rounded-lg mb-4"
         />
